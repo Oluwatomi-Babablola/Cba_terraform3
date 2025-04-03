a692c8ef1f696fb73de401509ae1820d1ba7f2b2
Oluwatomi-Babalola <boluwatomi01@gmail.com>
2025-04-03 18:25:02 +0100

    my first commit

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_ami" {
+  default = "ami-0faea58d4f6a5b206"
+}
+
+
+variable "vpc_id" {
+  default = "vpc-08a8742db45fcb0a8"
+}
+
+
+variable "key_name" {
+  default = "cba_keypairr"
+}
